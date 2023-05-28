725d3a7ecbd697e1cc4a997be8e6a2723476aa4b
stronyo2 <strony@o2.pl>
2023-05-28 15:22:53 +0200

    Added CameraScript
    Added CameraScript that controls video stream from the camera.

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScript : MonoBehaviour
+{
+    int currentCameraIndex = 0;
+    WebCamTexture tex;
+
+    public RawImage display;
+
+    public void SwamCam_Clicked()
+    {
+        currentCameraIndex += 1;
+        currentCameraIndex %= WebCamTexture.devices.Length;
+    }
+
+    public void StartStopCam_Clicked()
+    {
+        if(tex != null)
+        {
+            display.texture = null;
+            tex.Stop();
+            tex = null;
+        }
+        else
+        {
+            WebCamDevice device = WebCamTexture.devices[currentCameraIndex];
+            tex = new WebCamTexture(device.name);
+            display.texture = tex;
+
+            tex.Play();
+        }
+    }
+
+}
